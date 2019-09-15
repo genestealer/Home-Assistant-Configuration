a901a26a86f907c889b5068a8f943415041ed805
R Huish <5207490+genestealer@users.noreply.github.com>
2019-09-16 00:20:25 +0100

    Updates

--- esphome/chirpSensor.h
+++ esphome/chirpSensor.h
@@ -1,27 +1,69 @@
 #include "esphome.h"
-//#include "Wire.h"
-//#include <I2CSoilMoistureSensor.h>
+//  //#include "Wire.h"
+// //#include <I2CSoilMoistureSensor.h>
+//
+// //class ChirpCustomSensor : public PollingComponent, public Sensor {
+// class ChirpCustomSensor: public Component, public Sensor {
+//
+//   public: Sensor * capacitance_sensor = new Sensor();
+//   Sensor * temperature_sensor = new Sensor();
+//   //Sensor *light_sensor = new Sensor();
+//   //  I2CSoilMoistureSensor sensor;
+//   //ChirpCustomSensor() : PollingComponent(8000) { }
+//
+//   void setup() override {
+//     // Initialize the device here. Usually Wire.begin() will be called in here,
+//     // though that call is unnecessary if you have an 'i2c:' entry in your config
+//     Wire.setClockStretchLimit(5000); // https://github.com/Miceuz/i2c-moisture-sensor#note-for-esp8266-based-systems
+//     Wire.begin();
+//     Wire.setClockStretchLimit(5000); // https://github.com/Miceuz/i2c-moisture-sensor#note-for-esp8266-based-systems
+//
+//     // sensor.begin(); // reset sensor
+//     delay(1000); // give some time to boot up
+//   }
+//
+//   void writeI2CRegister8bit(int addr, int value) {
+//     Wire.beginTransmission(addr);
+//     Wire.write(value);
+//     Wire.endTransmission();
+//   }
+//
+//   unsigned int readI2CRegister16bit(int addr, int reg) {
+//     delay(1500);
+//     Wire.beginTransmission(addr);
+//     Wire.write(reg);
+//     Wire.endTransmission();
+//     delay(1100);
+//     Wire.requestFrom(addr, 2);
+//     unsigned int t = Wire.read() << 8;
+//     t = t | Wire.read();
+//     return t;
+//     delay(1100);
+//   }
+//   void loop() override {
+//
+//     //  void update() override {
+//     // This is the actual sensor reading logic.
+//     delay(1100);
+//     unsigned int capacitance = readI2CRegister16bit(0x20, 0); //read capacitance register
+//     capacitance_sensor -> publish_state(capacitance);
+//     //    unsigned int capacitance = sensor.getCapacitance(); //read capacitance register
+//     //    capacitance_sensor->publish_state(capacitance);
+//     delay(5000);
+//     // unsigned int temperature = readI2CRegister16bit(0x20, 5); //read temperature register
+//     // temperature_sensor - > publish_state(temperature);
+//
+//     //writeI2CRegister8bit(0x20, 3); //request light measurement
+//     //delay(9000);                   //this can take a while
+//     //unsigned int light = readI2CRegister16bit(0x20, 4);
+//     //light_sensor->publish_state(light);
+//   }
+// };
 
-
-//class ChirpCustomSensor : public PollingComponent, public Sensor {
-class ChirpCustomSensor : public Component, public Sensor {
-
+class MyCustomSensor : public PollingComponent, public Sensor {
  public:
-  Sensor *capacitance_sensor = new Sensor();
-  Sensor *temperature_sensor = new Sensor();
-  //Sensor *light_sensor = new Sensor();
-//  I2CSoilMoistureSensor sensor;
-  //ChirpCustomSensor() : PollingComponent(8000) { }
-
-  void setup() override {
-    // Initialize the device here. Usually Wire.begin() will be called in here,
-    // though that call is unnecessary if you have an 'i2c:' entry in your config
-	Wire.setClockStretchLimit(2500); // https://github.com/Miceuz/i2c-moisture-sensor#note-for-esp8266-based-systems
-	Wire.begin();
-	Wire.setClockStretchLimit(2500);
-//	sensor.begin(); // reset sensor
-//    delay(1000); // give some time to boot up
-  }
+  // constructor
+  MyCustomSensor() : PollingComponent(10000) {}
 
   void writeI2CRegister8bit(int addr, int value) {
     Wire.beginTransmission(addr);
@@ -35,26 +77,26 @@ class ChirpCustomSensor : public Component, public Sensor {
     Wire.endTransmission();
     delay(1100);
     Wire.requestFrom(addr, 2);
-   unsigned int t = Wire.read() << 8;
+    unsigned int t = Wire.read() << 8;
     t = t | Wire.read();
     return t;
   }
-  void loop() override {
 
-//  void update() override {
-    // This is the actual sensor reading logic.
-    
-	unsigned int capacitance = readI2CRegister16bit(0x20, 0); //read capacitance register
-    capacitance_sensor->publish_state(capacitance);
-//    unsigned int capacitance = sensor.getCapacitance(); //read capacitance register
-//    capacitance_sensor->publish_state(capacitance);
 
-    unsigned int temperature = readI2CRegister16bit(0x20, 5); //read temperature register
-    temperature_sensor->publish_state(temperature);
-    
-	//writeI2CRegister8bit(0x20, 3); //request light measurement
-    //delay(9000);                   //this can take a while
-    //unsigned int light = readI2CRegister16bit(0x20, 4);
-    //light_sensor->publish_state(light);
+  void setup() override {
+    // This will be called by App.setup()
+    Wire.setClockStretchLimit(5000); // https://github.com/Miceuz/i2c-moisture-sensor#note-for-esp8266-based-systems
+    Wire.begin();
+  }
+  void update() override {
+    // This will be called every "update_interval" milliseconds.
+    unsigned int capacitance = readI2CRegister16bit(0x20, 0); //read capacitance register
+
+    publish_state(capacitance); //read capacitance register
+    // writeI2CRegister8bit(0x20, 3); //request light measurement
+    // delay(9000);                   //this can take a while
+    // Serial.print(", ");
+    // Serial.println(readI2CRegister16bit(0x20, 4)); //read light register
+    // delay(500);
   }
 };
